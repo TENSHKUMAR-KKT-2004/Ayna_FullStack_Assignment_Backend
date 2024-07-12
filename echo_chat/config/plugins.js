68978e6ec6d4bb68da0344625cb08572cb5954c8
TENSHKUMAR K <105140296+TENSHKUMAR-KKT-2004@users.noreply.github.com>
2024-07-12 15:09:49 +0530

    Update plugins.js

--- echo_chat/config/plugins.js
+++ echo_chat/config/plugins.js
@@ -5,7 +5,7 @@ module.exports = ({env}) => ({
 			contentTypes: ['api::article.article'],
             socket: {
 				serverOptions: {
-					cors: { origin: 'https://echo-chat-2h3n.onrender.com/', methods: ['GET', 'POST'] },
+					cors: { origin: 'https://echo-chat-2h3n.onrender.com/*', methods: ['GET', 'POST'] },
 				},
 			},
 		},
